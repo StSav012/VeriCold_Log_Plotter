173b54aef2cf1b1eb69dbe66454a9aea436693f4
StSav012 <stsav012@gmail.com>
2022-01-29 21:34:31 +0100

    Improved ranging and simplified the menu

--- gui/_plot.py
+++ gui/_plot.py
@@ -3,10 +3,12 @@ from datetime import datetime, timedelta
 from itertools import cycle
 from typing import Any, List, Optional, Tuple, Iterable
 
+import numpy as np
 import pyqtgraph as pg
-from PySide6.QtCore import Qt, QDateTime, QEvent, QPointF, QRectF
-from PySide6.QtGui import QColor
+from PySide6.QtCore import Qt, QDateTime, QEvent, QPointF, QRectF, QCoreApplication
+from PySide6.QtGui import QColor, QAction
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QDateTimeEdit
+from pyqtgraph.GraphicsScene.mouseEvents import MouseClickEvent
 
 __all__ = ['Plot']
 
@@ -27,12 +29,68 @@ class Plot(QWidget):
         layout: QVBoxLayout = QVBoxLayout(self)
 
         plot: pg.PlotWidget = pg.PlotWidget(self)
+        self.lines: List[pg.PlotDataItem] = []
+
         self.canvas: pg.PlotItem = plot.getPlotItem()
+
+        def auto_range_y() -> None:
+            # self.canvas.vb.enableAutoRange(pg.ViewBox.YAxis)
+            if not self.lines:
+                return
+            line: pg.PlotDataItem
+            visible_data: List[np.ndarray] = []
+            x_min: float
+            x_max: float
+            y_min: float
+            y_max: float
+            [[x_min, x_max], [y_min, y_max]] = self.canvas.vb.viewRange()
+            for line in self.lines:
+                if line.yData is None or not line.yData.size:
+                    continue
+                visible_data_piece: np.ndarray = line.yData[(line.xData >= x_min) & (line.xData <= x_max)]
+                if np.any((visible_data_piece >= y_min) & (visible_data_piece <= y_max)):
+                    visible_data.append(visible_data_piece)
+            if not visible_data:
+                return
+            min_y: float = min(np.min(d) for d in visible_data if d.size)
+            max_y: float = max(np.max(d) for d in visible_data if d.size)
+            self.canvas.vb.setYRange(min_y, max_y, padding=0.0)
+
+        def on_view_all_triggered() -> None:
+            if not self.lines:
+                return
+            line: pg.PlotDataItem
+            min_x: float = min(line.xData[0] for line in self.lines if line.xData is not None and line.xData.size)
+            max_x: float = min(line.xData[-1] for line in self.lines if line.xData is not None and line.xData.size)
+            self.canvas.vb.autoRange(padding=0.0)
+            self.canvas.vb.setXRange(min_x, max_x, padding=0.0)
+
+        self.canvas.autoBtn.clicked.disconnect(self.canvas.autoBtnClicked)
+        self.canvas.autoBtn.clicked.connect(auto_range_y)
         self.canvas.setAxisItems({'bottom': pg.DateAxisItem()})
+
+        menu_action: QAction
+        for menu_action in self.canvas.ctrlMenu.actions():
+            if menu_action.text() in [
+                QCoreApplication.translate('PlotItem', 'Grid'),
+            ]:
+                self.canvas.vb.menu.addAction(menu_action)
+            else:
+                menu_action.deleteLater()
+        self.canvas.ctrlMenu = None
+
+        self.canvas.vb.disableAutoRange()
         self.canvas.vb.setAutoVisible(x=True, y=True)
         self.canvas.vb.setMouseMode(pg.ViewBox.RectMode)
         self.canvas.vb.setDefaultPadding(0.0)
+        self.canvas.vb.menu.axes[0].deleteLater()
+        self.canvas.vb.menu.ctrl[1].invertCheck.hide()
+        self.canvas.vb.menu.ctrl[1].label.hide()
+        self.canvas.vb.menu.ctrl[1].linkCombo.hide()
+        self.canvas.vb.menu.ctrl[1].mouseCheck.hide()
+        self.canvas.vb.menu.viewAll.triggered.connect(on_view_all_triggered)
         layout.addWidget(plot)
+
         cursor_balloon: pg.TextItem = pg.TextItem()
         plot.addItem(cursor_balloon, True)  # ignore bounds
 
@@ -96,14 +154,20 @@ class Plot(QWidget):
             cursor_balloon.setVisible(False)
             event.accept()
 
+        def on_mouse_clicked(event: MouseClickEvent) -> None:
+            if event.double():
+                auto_range_y()
+            event.accept()
+
         self._mouse_moved_signal_proxy: pg.SignalProxy = pg.SignalProxy(plot.scene().sigMouseMoved,
                                                                         rateLimit=10, slot=on_mouse_moved)
         self._axis_range_changed_signal_proxy: pg.SignalProxy = pg.SignalProxy(plot.sigRangeChanged,
                                                                                rateLimit=10, slot=on_lim_changed)
         self._last_time_range_rolled: datetime = datetime.fromtimestamp(0)
         plot.leaveEvent = on_plot_left
+        plot.scene().sigMouseClicked.connect(on_mouse_clicked)
 
-        def on_start_time_changed(new_time: QDateTime):
+        def on_start_time_changed(new_time: QDateTime) -> None:
             self.time_span.blockSignals(True)
             self.time_span.from_two_q_date_time(new_time, self.end_time.dateTime())
             self.time_span.blockSignals(False)
@@ -111,7 +175,7 @@ class Plot(QWidget):
                                      self.end_time.dateTime().toPython().timestamp(),
                                      padding=0.0)
 
-        def on_end_time_changed(new_time: QDateTime):
+        def on_end_time_changed(new_time: QDateTime) -> None:
             self.start_time.blockSignals(True)
             if new_time.addMSecs(-round(self.time_span.total_seconds * 1000)) >= self.start_time.minimumDateTime():
                 self.start_time.setDateTime(new_time.addMSecs(-round(self.time_span.total_seconds * 1000)))
@@ -125,7 +189,7 @@ class Plot(QWidget):
                                      self.end_time.dateTime().toPython().timestamp(),
                                      padding=0.0)
 
-        def on_time_span_changed(delta: timedelta):
+        def on_time_span_changed(delta: timedelta) -> None:
             self.start_time.blockSignals(True)
             if (self.end_time.dateTime().addMSecs(-round(delta.total_seconds() * 1000))
                     >= self.start_time.minimumDateTime()):
@@ -144,8 +208,6 @@ class Plot(QWidget):
         self.end_time.dateTimeChanged.connect(on_end_time_changed)
         self.time_span.timeSpanChanged.connect(on_time_span_changed)
 
-        self.lines: List[pg.PlotDataItem] = []
-
     def clear(self) -> None:
         self.canvas.clearPlots()
 
@@ -162,8 +224,8 @@ class Plot(QWidget):
                                                  cycle(colors or pg.CONFIG_OPTIONS['foreground']),
                                                  cycle(visibility or True)):
             y_column: int = data_model.header.index(y_column_name)
-            self.lines.append(self.canvas.plot(data_model[x_column], data_model[y_column],
-                                               pen=color))
+            self.lines.append(self.canvas.plot(data_model[x_column], data_model[y_column], pen=color))
+            self.lines[-1].curve.opts['pen'].setCosmetic(False)
             self.lines[-1].setVisible(visible)
 
         self.start_time.blockSignals(True)
@@ -176,7 +238,11 @@ class Plot(QWidget):
         self.end_time.blockSignals(False)
         self.start_time.blockSignals(False)
 
-        self.canvas.vb.autoRange(padding=0.0)
+        line: pg.PlotDataItem
+        min_y: float = min(np.min(line.yData) for line in self.lines if line.yData is not None and line.yData.size)
+        max_y: float = max(np.max(line.yData) for line in self.lines if line.yData is not None and line.yData.size)
+        self.canvas.vb.setYRange(min_y, max_y, padding=0.0)
+        self.canvas.vb.setXRange(data_model[x_column][0], data_model[x_column][-1], padding=0.0)
 
         self.start_time.setEnabled(bool(self.lines))
         self.end_time.setEnabled(bool(self.lines))
@@ -186,6 +252,7 @@ class Plot(QWidget):
                color: Optional[QColor] = None, roll: bool = False) -> None:
         if color is None:
             color = self.lines[index].opts['pen']
+        color.setCosmetic(False)
         x_column: int = data_model.header.index(x_column_name)
         y_column: int = data_model.header.index(y_column_name)
 
@@ -199,9 +266,7 @@ class Plot(QWidget):
             self.canvas.vb.setXRange(min(x_axis.range) + shift, max(x_axis.range) + shift, padding=0.0)
             self._last_time_range_rolled = datetime.now()
 
-        self.canvas.vb.disableAutoRange()
         self.lines[index].setData(data_model[x_column], data_model[y_column], pen=color)
 
     def set_line_visible(self, index: int, visible: bool) -> None:
-        self.canvas.vb.disableAutoRange()
         self.lines[index].setVisible(visible)
